2c1d55fad53243bd145cdadecbdd736c635d82df
mooseburger <carlosfabianramirez@gmail.com>
2015-08-23 15:15:27 -0400

    Plaza Las Americas sales scraping and DB storing complete!

--- sale-scraper.js
+++ sale-scraper.js
@@ -19,25 +19,73 @@ request(url, function (error, response, body) {
 function ScrapeSale (url) {
 	request(url, function (error, response, body) {
 		if (!error && response.statusCode === 200) {
-			var imgUrl = $(body).find('#generic-middle').children('img.attachment-full').attr('src');
 
-			var splitUrl = url.split('/');
+			var saleDiv = $(body).find('#generic-middle');
 
-			DownloadImage(splitUrl[splitUrl.length - 2] ,imgUrl);
+			var sale = {};
+
+			sale.name = saleDiv.children('.promo-title').text();
+			sale.description = saleDiv.children('p').text();
+
+			var saleDates = saleDiv.children('.promo-title + h3').text().split(' - ');
+			sale.effective_date = saleDates[0];
+			sale.expiration_date = saleDates.length === 2 ? saleDates[1] : '';
+
+			var imgUrl = saleDiv.children('img.attachment-full').attr('src');
+
+			sale.image = GetImagePath(url, imgUrl);
+
+			InsertSale(sale);
+
+			DownloadImage(imgUrl, sale.image);
 		}
 
 		else {
-			console.log(error);
+			LogError(error, response, body);
 		}
 	});
 }
 
-function DownloadImage(imgName, imgUrl) {
+function InsertSale(sale) {
+	request.post(
+		{
+			url: 'http://localhost:3000/api/Sales',
+			body: sale,
+			json: true
+		}, LogError);
+}
+
+function DownloadImage(imgUrl, imgPath) {
+	if (imgPath) {
+		request(imgUrl, LogError).pipe(fs.createWriteStream(imgPath));
+	}
+}
+
+function GetImagePath(pageUrl, imgUrl) {
 	if (imgUrl) {
+
+		var splitUrl = pageUrl.split('/');
+		var imgName = splitUrl[splitUrl.length - 2];
 
 		var splitExt = imgUrl.split('.');
 		var imgExtension = splitExt[splitExt.length - 1];
 
-		request(imgUrl).pipe(fs.createWriteStream('../Pictures/' + imgName + '.' + imgExtension));
+		return '../Pictures/' + imgName + '.' + imgExtension;
+	}
+
+	else {
+		LogError(pageUrl, 'No image found');
+
+		return '';
+	}
+}
+
+function LogError (error, response, body) {
+	if (error || response.statusCode !== 200) {
+
+		error = error ? error : 'No error returned';
+
+		var logFile = fs.createWriteStream('../debug.log', { flags : 'a' });
+		logFile.write(error + ':\n' + JSON.stringify(response) + '\n\n');
 	}
 }
