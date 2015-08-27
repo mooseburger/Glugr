d763d15f40898ffff038234778439f4086134615
mooseburger <carlosfabianramirez@gmail.com>
2015-08-26 22:02:48 -0400

    Dopetrope template added. Can now serve static files.

--- common/models/sales.js
+++ common/models/sales.js
@@ -1,3 +1,17 @@
-module.exports = function(Sales) {
+module.exports = function (Sales) {
+	Sales.saleName = function (id, cb) {
 
+		Sales.findById(id, function (err, instance) {
+			cb(null, instance.name);
+		});
+	};
+
+	Sales.remoteMethod(
+		'saleName',
+		{
+			http: { path: '/saleName', verb: 'get'},
+			accepts: { arg: 'id', type: 'number', http: { source: 'query'} },
+			returns: { arg: 'name', type: 'string' }
+		}
+	);
 };
