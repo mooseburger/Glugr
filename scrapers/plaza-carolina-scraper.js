34070de734eabb9acd90b09c7b3c13dbd27fc4ba
mooseburger <carlosfabianramirez@gmail.com>
2015-09-13 16:29:01 -0400

    Now using Promis.all

--- scrapers/plaza-carolina-scraper.js
+++ scrapers/plaza-carolina-scraper.js
@@ -4,17 +4,23 @@ var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
 
 var malls = require('./mall-enum');
 
-var url = 'http://www.simon.com/mall/plaza-carolina/deals';
+var domain = 'http://www.simon.com';
+var saleListing = domain + '/mall/plaza-carolina/deals';
 
 var timeBetweenRequests = 0;
 
-var loadedPage = PoliteRequest(url, timeBetweenRequests);
+var loadedPage = PoliteRequest(saleListing, timeBetweenRequests);
 
 loadedPage.then(function (body) {
-	$(body).find('#content-stream-container').children('.CSItem').children('span:nth-child(1)').each(function () {
-		console.log($(this).data('tenname'));
+
+	var saleRequests = [];
+
+	$(body).find('#content-stream-container').children('.CSItem').children('a:nth-child(2)').each(function () {
+		saleRequests.push(PoliteRequest(domain + $(this).attr('href'), timeBetweenRequests += 2000));
 	});
-});
+
+	return Promise.all(saleRequests);
+}).catch(console.log.bind(console));
 
 function PoliteRequest(url, delay) {
 	return new Promise(function (resolve, reject) {
