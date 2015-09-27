3defe801e5d7e5b2d5da5bd484dc1f3f35edb4a3
mooseburger <carlosfabianramirez@gmail.com>
2015-09-27 18:00:59 -0400

    Mall filter coming along!

--- common/models/mall.js
+++ common/models/mall.js
@@ -1,3 +1,15 @@
 module.exports = function(Mall) {
+	Mall.promisoryFind = function (filter) {
 
+		return new Promise(function (resolve, reject) {
+
+			Mall.find(filter, function (error, malls) {
+				if (error) {
+					return reject(error);
+				}
+
+				resolve(malls);
+			});
+		});
+	}
 };
---
+++ components/MallFilter.js
@@ -0,0 +1,35 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var MallFilterButton = React.createClass({
+	render: function () {
+
+		var selectedButton = this.props.mallId === 1 ? 'current' : '';
+
+		return (
+			<li className={selectedButton}><a href="#">{this.props.name}</a></li>
+		);
+	}
+});
+
+var MallFilter = React.createClass({
+	render: function () {
+
+		var filterButtons = this.props.malls.map(function (mall) {
+			return (
+				<MallFilterButton key={mall.id} mallId={mall.id} name={mall.name} />
+			);
+		});
+
+		return (
+			<nav className="nav">
+				<ul>
+					{filterButtons}
+				</ul>
+			</nav>
+		);
+	}
+});
+
+module.exports = MallFilter;
