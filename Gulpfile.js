082b89c8b714b36ad5394cf0d9a1e7db1b4a21bf
mooseburger <carlosfabianramirez@gmail.com>
2015-09-29 17:30:11 -0400

    Minified Glugr app

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,31 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+'use strict';
 
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+//RECIPE
+//https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-uglify-sourcemap.md
 gulp.task('scripts', function () {
 
-	gulp.src(['client/assets/js/glugr.js'])
-		.pipe(browserify({
-			debug: true,
-			transform: ['reactify']
-		}))
-		.pipe(gulp.dest('client/assets/js/bundles'));
+	var b = browserify({
+		entries: './client/assets/js/glugr.js',
+		debug: true,
+		transform: ['reactify']
+	});
+
+	return b.bundle()
+		.pipe(source('glugr.min.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(uglify())
+		.on('error', gutil.log)
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./client/assets/js/bundles/'));
 });
 
 gulp.task('default', ['scripts']);
