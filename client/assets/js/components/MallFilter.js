ebd6c67cd67d900b7e9d7e4ab04bf8043d0c5d2c
mooseburger <carlosfabianramirez@gmail.com>
2015-09-29 23:17:50 -0400

    Nearly done setting up client side React

--- client/assets/js/components/MallFilter.js
+++ client/assets/js/components/MallFilter.js
@@ -3,27 +3,55 @@
 var React = require('react');
 
 var MallFilterButton = React.createClass({
+	isSelectedMall: function () {
+		return this.props.mall.id === this.props.selectedMall;
+	},
 	render: function () {
 
-		var selectedButton = this.props.mallId === 1 ? 'current' : '';
+		var selectedButton = this.isSelectedMall() ? 'current' : '';
 
 		return (
-			<li className={selectedButton}><a href="#">{this.props.name}</a></li>
+			<li className={selectedButton}>
+				<a href="#" data-id={this.props.mall.id}>
+					{this.props.mall.name}
+				</a>
+			</li>
 		);
 	}
 });
 
 var MallFilter = React.createClass({
+	handleFilter: function (e) {
+		var clickedEl = e.target;
+		console.log('event fired!');
+		if (clickedEl.tagName === 'a') {
+			console.log(clickedEl.dataset.id);
+			e.preventDefault();
+
+			var mallId = parseInt(clickedEl.dataset.id);
+
+			if (mallId !== this.props.mallId) {
+				this.props.filterSales(mallId);
+			}
+		}
+	},
 	render: function () {
+
+		var selectedMall = this.props.mallId;
+
 		var filterButtons = this.props.malls.map(function (mall) {
 			return (
-				<MallFilterButton key={mall.id} mallId={mall.id} name={mall.name} />
+				<MallFilterButton
+					key={mall.id}
+					mall={mall}
+					selectedMall={selectedMall}
+				/>
 			);
 		});
 
 		return (
 			<nav className="nav">
-				<ul>
+				<ul onClick={this.handleFilter}>
 					{filterButtons}
 				</ul>
 			</nav>
